0496be4e4142e1b3c4167ec41e1f45c2be3c87b0
Hemanthkumar <hemanthkumarkv153@gmail.com>
2026-03-24 13:57:07 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -1,17 +1,42 @@
 
 const express = require("express")
-
 const app = express()
 
+// app.use("/", (req, res)=>{
+//     res.send("Hello from the Server ")
+// })
 
-app.use((req, res)=>{
-    res.send("Hello from the Server ")
+// http://localhost:3000/hello ----->  Hello from the Server
+// http://localhost:3000/hello/2 ----->  Hello from the Server
+
+app.use("/hello/2", (req, res)=>{
+    res.send("Hello - 2")
 })
+
+// http://localhost:3000/hello/2  --->  Hello - 2
+// http://localhost:3000/hello ---> Hello - 1
+
+
+app.use("/hello", (req, res)=>{
+    res.send("Hello - 1")
+})
+
+
+app.get("/user/:userId/:password", (req, res)=>{
+    console.log(req.params)
+    res.send("Hello - 2")
+})
+
+// http://localhost:3000/user/10/Hemanth@#1996
+// o/p: Hello - 2
+
 
 app.listen(3000, ()=>{
     console.log("Server running of the port 3000")
 })
 
+
+// http://localhost:3000/hello/2 --->
 
 
 console.log("Hemanth kumar kv")
